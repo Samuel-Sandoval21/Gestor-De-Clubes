526f71b746a7d7415d3b6c8b50d6b28929a5890c
Samuel Sandoval <ssandoval40716@ufide.ac.cr>
2026-08-12 23:41:29 -0600

    Commit 1-Prototipo
    Prototipo

--- GestorDeClubes.MVC/Controllers/HomeController.cs
+++ GestorDeClubes.MVC/Controllers/HomeController.cs
@@ -4,21 +4,24 @@ using GestorDeClubes.MVC.Models;
 
 namespace GestorDeClubes.MVC.Controllers;
 
-public class HomeController : Controller
-{
-    public IActionResult Index()
-    {
-        return View();
-    }
+using Microsoft.AspNetCore.Mvc;
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+
+    public class HomeController : Controller
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Index()
+        {
+            // SIMULACIÓN: El rol se obtendría de la base de datos (Identity o tabla Usuario)
+            // Cambia esto manualmente para probar diferentes vistas:
+            // "Estudiante", "Coordinador", "Bienestar", "Admin"
+            ViewBag.UserRole = "Estudiante";
+
+            // Datos simulados para el Dashboard (Estadísticas)
+            ViewBag.ClubesActivos = 12;
+            ViewBag.EventosProximos = 4;
+            ViewBag.MiembrosTotales = 185;
+
+            return View();
+        }
     }
-}
---
+++ GestorDeClubes.MVC/Controllers/PrototipoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestorDeClubes.MVC.Controllers
+{
+    public class PrototipoController : Controller
+    {
+        // Vista 1: Lista de Clubes Activos
+        public IActionResult Clubes()
+        {
+            // Simulamos una lista de clubes para el prototipo
+            ViewBag.Clubes = new List<string> { "Club de Fotografía", "Club de Ajedrez", "Club de Robótica", "Club de Debate", "Club de Volleyball" };
+            return View();
+        }
+
+        // Vista 2: Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // Vista 3: Próximos Eventos (El calendario que ya diseñaste)
+        // Nota: Ponemos el nombre "Eventos" para que el menú sea claro
+        public IActionResult Eventos()
+        {
+            return View();
+        }
+    }
+}
